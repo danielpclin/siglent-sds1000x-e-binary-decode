fa2b575ea003d46aa4888a48e9a529e0dd60d07e
danielpclin <daniel.pclin@gmail.com>
2023-10-08 16:21:07 +0800

    add verbosity

--- decode.py
+++ decode.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import struct
 from zipfile import ZipFile
+import logging as log
 
 import numpy as np
 
@@ -13,46 +14,45 @@ def magnitude_to_decimal(index: int):
     return 10 ** ((index - 8) * 3)
 
 
-def decode(filename="SDS00001.bin", verbose=True):
-    # TODO: add verbosity logging
+def decode(filename="SDS00001.bin"):
     with open(f"{filename}", 'rb') as file:
         ch_on = struct.unpack('<4i', file.read(4 * 4))
-        print(f"{ch_on = }")
+        log.info(f"{ch_on = }")
 
         ch_volt_div = tuple(map(lambda x: (x[0] * magnitude_to_decimal(x[1]), UNITS_TABLE[x[2]]),
                                 struct.iter_unpack('<dII', file.read(16 * 4))))
-        print(f"{ch_volt_div = }")
+        log.info(f"{ch_volt_div = }")
 
         ch_vert_offset = tuple(map(lambda x: (x[0] * magnitude_to_decimal(x[1]), UNITS_TABLE[x[2]]),
                                    struct.iter_unpack('<dII', file.read(16 * 4))))
-        print(f"{ch_vert_offset = }")
+        log.info(f"{ch_vert_offset = }")
 
         digital_on = struct.unpack('<I', file.read(4))[0]
-        print(f"{digital_on = }")
+        log.info(f"{digital_on = }")
 
         d_on = struct.unpack('<16I', file.read(4 * 16))
-        print(f"{d_on = }")
+        log.info(f"{d_on = }")
 
         time_div = struct.unpack('<dII', file.read(16))
         time_div = (time_div[0] * magnitude_to_decimal(time_div[1]), UNITS_TABLE[time_div[2]])
-        print(f"{time_div = }")
+        log.info(f"{time_div = }")
 
         time_delay = struct.unpack('<dII', file.read(16))
         time_delay = (time_delay[0] * magnitude_to_decimal(time_delay[1]), UNITS_TABLE[time_delay[2]])
-        print(f"{time_delay = }")
+        log.info(f"{time_delay = }")
 
         wave_length = struct.unpack('<I', file.read(4))[0]
-        print(f"{wave_length = }")
+        log.info(f"{wave_length = }")
 
         sample_rate = struct.unpack('<dII', file.read(16))
         try:
             sample_rate = (sample_rate[0] * magnitude_to_decimal(sample_rate[1]), UNITS_TABLE[sample_rate[2]])
         except IndexError:
             sample_rate = (sample_rate[0] * magnitude_to_decimal(sample_rate[1]), f"N/A ({sample_rate[2]})")
-        print(f"{sample_rate = }")
+        log.info(f"{sample_rate = }")
 
         digital_wave_length = struct.unpack('<I', file.read(4))[0]
-        print(f"{digital_wave_length = }")
+        log.info(f"{digital_wave_length = }")
 
         digital_sample_rate = struct.unpack('<dII', file.read(16))
         try:
@@ -61,14 +61,14 @@ def decode(filename="SDS00001.bin", verbose=True):
         except IndexError:
             digital_sample_rate = (
             digital_sample_rate[0] * magnitude_to_decimal(digital_sample_rate[1]), f"N/A ({digital_sample_rate[2]})")
-        print(f"{digital_sample_rate = }")
+        log.info(f"{digital_sample_rate = }")
 
         ch_attenuation = struct.unpack('<4d', file.read(8 * 4))
-        print(f"{ch_attenuation = }")
+        log.info(f"{ch_attenuation = }")
 
         time = (time_delay[0] - (time_div[0] * 7), 1 / sample_rate[0])
         time = (time[0], time[1], sample_rate[0], np.arange(time[0], time[0] + wave_length * time[1], time[1]))
-        print(f"{time = }")
+        log.info(f"{time = }")
         analog_data = []
         digital_data = []
         file.seek(0x800, 0)
@@ -79,7 +79,7 @@ def decode(filename="SDS00001.bin", verbose=True):
 
                 ch_data = ((ch_data - 128) * ch_volt_div[0][0] / 25 - ch_vert_offset[0][0]) * ch_attenuation[i]
                 ch_data = ch_data.astype(np.dtype(np.float32).newbyteorder("<"))
-                print(f"ch{i + 1} data:", ch_data)
+                log.info(f"ch{i + 1} data: {ch_data}")
                 analog_data.append(ch_data)
             else:
                 analog_data.append(None)
@@ -90,7 +90,7 @@ def decode(filename="SDS00001.bin", verbose=True):
                     ch_data = np.frombuffer(
                         struct.unpack(f'<{int(digital_wave_length)}s', file.read(int(digital_wave_length)))[0],
                         dtype=np.uint8)
-                    print(f"ch{i + 1} data:", ch_data)
+                    log.info(f"ch{i + 1} data: {ch_data}")
                     digital_data.append(ch_data)
                 else:
                     digital_data.append(None)
@@ -133,17 +133,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="binary file to decode")
     parser.add_argument("-r", "--raw", help="write to raw binary file instead of sigrok zip file", action='store_true')
-    parser.add_argument("-v", "--verbose", help="print verbose logs", action='store_true')  # TODO
+    parser.add_argument("-v", "--verbose", help="print verbose logs", action='store_true')
     args = parser.parse_args()
+    if args.verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
 
-    analog_data, digital_data, time = decode(args.filename, verbose=args.verbose)
+    analog_data, digital_data, time = decode(args.filename)
     if args.raw:
         for i, data in enumerate(analog_data):
             if data is not None:
                 save_analog_to_file(data, f"{os.path.splitext(args.filename)[0]}-ch{i+1}.bin")
         # TODO: write digital data
+        print("Raw file saved!")
     else:
-        save_to_sigrok_zip(analog_data, digital_data, time[2], os.path.splitext(args.filename)[0] + ".sr")
+        filename = f"{os.path.splitext(args.filename)[0]}.sr"
+        save_to_sigrok_zip(analog_data, digital_data, time[2], filename)
+        print(f"Sigrok file saved as {filename}")
 
 
 if __name__ == "__main__":
